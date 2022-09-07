4f80e95b8e34b33a6e1caaaabc7b3a121c883fcf
Kota <maedakoutaa@gmail.com>
2022-09-07 14:10:34 +0900

    Initial Commit

---
+++ MiniApp91-SwiftUI-ChangingTopView/MiniApp91_SwiftUI_ChangingTopViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp91_SwiftUI_ChangingTopViewApp.swift
+//  MiniApp91-SwiftUI-ChangingTopView
+//
+//  Created by 前田航汰 on 2022/09/07.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp91_SwiftUI_ChangingTopViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
